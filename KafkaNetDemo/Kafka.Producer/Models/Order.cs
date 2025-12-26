e5d5f623189152898860400bf396544525891796
Paiva <danpaiva@live.com>
2025-12-26 17:56:09 -0300

    feat: add logica inicial e modelo de Order

---
+++ KafkaNetDemo/Kafka.Producer/Models/Order.cs
@@ -0,0 +1,3 @@
+﻿namespace Kafka.Producer.Models;
+
+public record Order(int Id, string Product, decimal Price, DateTime CreatedAt);
--- KafkaNetDemo/Kafka.Producer/Program.cs
+++ KafkaNetDemo/Kafka.Producer/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+using Kafka.Producer.Models;
+using System.Net;
+using System.Text.Json;
+
+var config = new ProducerConfig { 
+    BootstrapServers = "localhost:9092", 
+    ClientId = Dns.GetHostName() };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+
+Console.WriteLine("--- Gerador de Pedidos (Producer) ---");
+
+for (int i = 1; i <= 5; i++)
+{
+    var order = new Order(i, $"Produto {i}", i * 15.5m, DateTime.Now);
+    var json = JsonSerializer.Serialize(order);
+
+    var message = new Message<string, string>
+    {
+        Key = order.Id.ToString(), // Chave é importante para garantir ordem no Kafka
+        Value = json
+    };
+
+    var result = await producer.ProduceAsync("vendas-pedidos", message);
+    Console.WriteLine($"[Enviado] Pedido {order.Id} - Offset: {result.Offset}");
+
+    await Task.Delay(1000); // Simula um intervalo entre vendas
+}
