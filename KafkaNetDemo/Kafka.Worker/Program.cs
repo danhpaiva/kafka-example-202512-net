ae27b8805143690df580fdb3a53c94556f04125d
Paiva <danpaiva@live.com>
2025-12-26 19:35:23 -0300

    feat: add RetryWorker na Program.cs

--- KafkaNetDemo/Kafka.Worker/Program.cs
+++ KafkaNetDemo/Kafka.Worker/Program.cs
@@ -3,5 +3,7 @@ using Kafka.Worker.Workers;
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<KafkaWorker>();
 
+builder.Services.AddHostedService<KafkaRetryWorker>();
+
 var host = builder.Build();
 host.Run();
