a7165b9910817d9ba1d381a45c9c261915c27adb
Paiva <danpaiva@live.com>
2025-12-26 18:02:33 -0300

    feat: add projeto consumer

---
+++ KafkaNetDemo/Kafka.Consumer/Models/Order.cs
@@ -0,0 +1,3 @@
+﻿namespace Kafka.Consumer.Models;
+
+public record Order(int Id, string Product, decimal Price, DateTime CreatedAt);
---
+++ KafkaNetDemo/Kafka.Consumer/Program.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+using Kafka.Consumer.Models;
+using System.Text.Json;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "vendas-processor-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+consumer.Subscribe("vendas-pedidos");
+
+Console.WriteLine("--- Processador de Pedidos (Consumer) ---");
+
+while (true)
+{
+    var result = consumer.Consume();
+    var order = JsonSerializer.Deserialize<Order>(result.Message.Value);
+
+    Console.WriteLine($"[Processando] ID: {order?.Id} | Produto: {order?.Product} | Total: R$ {order?.Price}");
+}
